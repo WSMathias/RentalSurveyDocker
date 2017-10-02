9b32bb35a101f8abcca6a5e2ad678e2477db34ad
WSMathias <wsmsamuel@gmail.com>
2017-10-02 19:56:24 +0530

    class SurveyForm added

---
+++ app/formclass.php
@@ -0,0 +1,21 @@
+<?php
+class Surveyform {
+
+    private $fields = array();
+
+    function __construct($fields) {
+        $this->$fields = $fields;
+    }
+
+    public function render() {
+    }
+
+    public function validate() {
+    }
+
+    public function submit() {
+    }
+
+
+}
+?>
