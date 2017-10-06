91ae52ac14fd4b1cecd064124228d8f9b359a1d9
Rahul Sarkar <rahul.sarkar@srijan.net>
2017-10-06 12:17:56 +0530

    Adding the util directory to the fpm container for dumping *.sql file into databse

---
+++ fpm/util/default.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE IF NOT EXISTS `details` (
+  `ID` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `AREA` float(20,5) DEFAULT NULL,
+  `PRICE` float(20,5) DEFAULT NULL,
+  `DEPOSIT` float(30,5) DEFAULT NULL,
+  `LEASE_PERIOD` int(11) DEFAULT NULL,
+  `Lid` bigint(20) NOT NULL
+) ;
+
+CREATE TABLE IF NOT EXISTS `places` (
+  `id` bigint(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `location` varchar(150) DEFAULT NULL
+) ;
+
+CREATE TABLE IF NOT EXISTS `survey` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `location` varchar(500) DEFAULT NULL,
+  `area` float(20,5) DEFAULT NULL,
+  `price` float(20,5) DEFAULT NULL,
+  `deposit` float(20,5) DEFAULT NULL,
+  `lease_period` int(11) DEFAULT NULL
+);
